20620b33dc952dc225b8beada56117efcb1a9322
Rian <rianrbps@gmail.com>
2024-05-21 22:49:12 -0300

    routes

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const { syncDatabase } = require('./models');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-const PORT = 5432;
+const PORT = 5433;
 
 app.use(bodyParser.json());
-app.use('/users', userRoutes);
+//app.use('/users');
 
 syncDatabase().then(() => {
     app.listen(PORT, ()=> {
         console.log('Servidor está rodando na em http://localhost:${PORT}');
     });
-});
+})
