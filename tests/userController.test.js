91534d91e8f6771de95afba214255269f569bac9
Rian <rianrbps@gmail.com>
2024-06-06 16:20:21 -0300

    Tests fixed

--- tests/userController.test.js
+++ tests/userController.test.js
@@ -102,26 +102,10 @@ describe("user controller", () => {
   });
 
   describe("updates a user", () => {
-    let req, res;
-    beforeEach(() => {
-        req = {
-            body: {},
-            param: {},
-        };
-        res = {
-            status: jest.fn().mockReturnThis(),
-            json: jest.fn().mockReturnThis,
-        }
-    });
-    afterEach(() => {
-        jest.resetAllMocks();
-    });
-    describe("updates an user", () => {
-        it("updates an user if the user alreadys exists");
-        it("fails to update an inexisting user");
-        it("tries to update an user with missing fields requirements");
-        it("handles database error");
-    })
+      it("updates an user if the user alreadys exists");
+      it("fails to update an inexisting user");
+      it("tries to update an user with missing fields requirements");
+      it("handles database error");
   });
 
 });
